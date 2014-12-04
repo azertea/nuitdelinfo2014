41d85dd362dca4363e1ed4ae04accc10653f0812
YseultB <yseult.bouillon@free.fr>
2014-12-04 23:45:03 +0100

    Ajout des classes du Modèle

---
+++ site/core/database/Profil.php
@@ -0,0 +1,104 @@
+<?php
+
+	/**
+	 * Classe correspondante à un profil utilisateur
+	 * @author Yseult B., Malik I.
+	 */
+
+	/* ******************************************************
+	 * 	Attributs
+	 * ******************************************************/
+	private idProfil;
+	private nom;
+	private prenom;
+	private descPhysique;
+	private localisation;
+	private telephone;
+	private idRefuge;
+	private idUser;
+
+
+	public function __construct($idProfil, $nom, $prenom,
+								$descPhysique, $localisation,
+								$telephone, $idRefuge, $idUser) {
+		$this->idProfil = $idProfil;
+		$this->nom = $nom;
+		$this->prenom = $prenom;
+		$this->descPhysique = $descPhysique;
+		$this->localisation = $localisation;
+		$this->telephone = $telephone;
+		$this->idRefuge = $idRefuge;
+		$this->idUser = $idUser;
+	}
+
+
+	/* ******************************************************
+	 * 	Getters
+	 * ******************************************************/
+
+	public function getIdProfil() {
+		return $this->idProfil;
+	}
+
+	public function getNom() {
+		return $this->nom;
+	}
+
+	public function getPrenom() {
+		return $this->prenom;
+	}
+
+	public function getDescPhysique() {
+		return $this->descPhysique;
+	}
+
+	public function getLocalisation() {
+		return $this->localisation;
+	}
+
+	public function getTelephone() {
+		return $this->telephone;
+	}
+
+	public function getIdRefuge() {
+		return $this->idRefuge;
+	}
+
+	public function getIdUser() {
+		return $this->idUser;
+	}
+
+
+
+	/* ******************************************************
+	 * 	Setters
+	 * ******************************************************/
+
+	public function setNom($nom) {
+		$this->nom = $nom;
+	}
+
+	public function setPrenom($prenom) {
+		$this->prenom = $prenom;
+	}
+
+	public function setDescPhysique($descPhysique) {
+		$this->descPhysique = $descPhysique;
+	}
+
+	public function setLocalisation($localisation) {
+		$this->localisation = $localisation;
+	}
+
+	public function setTelephone($telephone) {
+		$this->telephone = $telephone;
+	}
+
+	public function setIdRefuge($idRefuge) {
+		$this->idRefuge = $idRefuge;
+	}
+
+	public function setIdUser($idUser) {
+		$this->idUser = $idUser;
+	}
+?>
