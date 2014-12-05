e4c944a20c1163b787788c7da5df238f5885859e
Florentin Le Moal <florentin.lemoal@gmail.com>
2014-12-05 01:59:46 +0100

    Fonction de recherche depuis un utilisateur public ou une ONG avec envoi d'un mail à la personne concernée.

--- site/core/includes/service.php
+++ site/core/includes/service.php
@@ -235,13 +235,160 @@ function listeProfil()
 	return $listProfile;
 }
 
-function rechercheProfil($keywordDesc, $keywordLoc)
+// Retourne :
+//  - (SER_ERR_EMPTY_PARAM_SEARCH) si tous les champs sont vides
+//  - (SER_ERR_DB) si problème avec la base
+//  - Le nombre de personnes quiont étés contactées suite à la recherche
+function rechercheProfilPublic($nom, $prenom, $desc, $localisation, $telephone)
 {
-	# code...
+	$listProfile = array();
+	$listMail = array();
+
+	if (empty($nom) && empty($prenom) && empty($desc) && empty($localisation) && empty($telephone)) {
+		return $SER_ERR_EMPTY_PARAM_SEARCH;
+	}
+
+	try {
+		db_open();
+		$listProfile = db_getSearchProfile($nom, $prenom, $desc, $localisation, $telephone);
+
+		// Contacter la liste des personnes
+		foreach ($listProfile as $key => $profil) {
+			$user = db_getUserFromProfile($profil);
+			array_push($listMail, $user->getEmail());
+		}
+
+		db_close();
+	} catch (Exception $e) {
+		return $SER_ERR_DB;
+	}
+
+	foreach ($listMail as $key => $mail) {
+		envoyerMailFromPublic($mail);
+	}
+
+	return count($listProfile);
 }
 
-function serv_modificationProfil($new_nom, $new_prenom, $new_desc, $new_localisation, $new_telephone)
+// Retourne :
+//  - (SER_ERR_EMPTY_PARAM_SEARCH) si tous les champs sont vides
+//  - (SER_ERR_DB) si problème avec la base
+//  - La liste des profils contactés suite à la recherche
+function rechercheProfilONG($nom, $prenom, $desc, $localisation, $telephone)
 {
+	$listProfile = array();
+	$listMail = array();
 
+	if (empty($nom) && empty($prenom) && empty($desc) && empty($localisation) && empty($telephone)) {
+		return $SER_ERR_EMPTY_PARAM_SEARCH;
+	}
+
+	try {
+		db_open();
+		$listProfile = db_getSearchProfile($nom, $prenom, $desc, $localisation, $telephone);
+
+		// Contacter la liste des personnes
+		foreach ($listProfile as $key => $profil) {
+			$user = db_getUserFromProfile($profil);
+			array_push($listMail, $user->getEmail());
+		}
+
+		db_close();
+	} catch (Exception $e) {
+		return $SER_ERR_DB;
+	}
+
+	foreach ($listMail as $key => $mail) {
+		envoyerMailFromONG($mail);
+	}
+
+	return $listProfile;
+}
+
+
+////////////////////////////////////////
+/////////// Fonction privées ///////////
+
+function envoyerMailFromPublic($mailDest)
+{
+	$user = $_SESSION['user'];
+	$mailSource = $user->getEmail();
+
+	$content = "un utilisateur public (" . $mailSource . ") vous a recherché.".
+	envoyerMail($content, $mailDest)
+}
+
+function envoyerMailFromONG($mailDest)
+{
+	$user = $_SESSION['user'];
+	$mailSource = $user->getEmail();
+
+	$content = "une ONG (" . $mailSource . ") vous a recherché.".
+	envoyerMail($content, $mailDest)
+}
+
+function envoyerMail($content, $mailDest)
+{
+	$user = $_SESSION['user'];
+	$mailSource = $user->getEmail();
+	$loginSource = $user->getLogin();
+	
+	if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mailDest)) // On filtre les serveurs qui présentent des bogues.
+	{
+		$passage_ligne = "\r\n";
+	}
+	else
+	{
+		$passage_ligne = "\n";
+	}
+	//=====Déclaration des messages au format texte et au format HTML.
+	$message_txt = "Bonjour, " . $content;
+	$message_html = "<html><head></head><body><b>Bonjour</b>, " . $content. ".</body></html>";
+	//==========
+
+	//=====Création de la boundary.
+	$boundary = "-----=".md5(rand());
+	$boundary_alt = "-----=".md5(rand());
+	//==========
+	 
+	//=====Définition du sujet.
+	$sujet = "Recherché par " . $mailSource;
+	//=========
+	 
+	//=====Création du header de l'e-mail.
+	$header = "From: \"" . $loginSource . "\" <" . $mailSource . ">" . $passage_ligne;
+	$header.= "Reply-to: \"" . $loginSource . "\" <" . $mailSource . ">" . $passage_ligne;
+	$header.= "MIME-Version: 1.0".$passage_ligne;
+	$header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+	//==========
+	 
+	//=====Création du message.
+	$message = $passage_ligne."--".$boundary.$passage_ligne;
+	$message.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary_alt\"".$passage_ligne;
+	$message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
+	//=====Ajout du message au format texte.
+	$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+	$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+	$message.= $passage_ligne.$message_txt.$passage_ligne;
+	//==========
+	 
+	$message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
+	 
+	//=====Ajout du message au format HTML.
+	$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+	$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+	$message.= $passage_ligne.$message_html.$passage_ligne;
+	//==========
+	 
+	//=====On ferme la boundary alternative.
+	$message.= $passage_ligne."--".$boundary_alt."--".$passage_ligne;
+	//==========
+	 
+	 
+	 
+	$message.= $passage_ligne."--".$boundary.$passage_ligne;
+	 
+	//=====Envoi de l'e-mail.
+	mail($mailDest, $sujet, $message, $header);
 }
 ?>
